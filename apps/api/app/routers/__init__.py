761a51594446bde3969cc29bf033175577b563d1
JJ Tsao <35306235+jj-tsao@users.noreply.github.com>
2025-10-02 10:38:07 -0700

    update

--- apps/api/app/routers/__init__.py
+++ apps/api/app/routers/__init__.py
@@ -1,6 +1,6 @@
-from .taste_profile import router as taste_profile_router
-# from .discover import router as discover_router
-from .recommend import router as recommend_router
+from .routes_taste_profile import router as taste_profile_router
+from .routes_recommend import router as recommend_router
+# from .routes_discover import router as discover_router
 # from .profile import router as profile_router
 
 all_routers = [taste_profile_router, recommend_router]
