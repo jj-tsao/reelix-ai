8381d39b88cdf995260072fdfb1d6fdbc4daaa75
JJ Tsao <35306235+jj-tsao@users.noreply.github.com>
2025-12-03 16:54:30 -0800

    update qdrant collection

--- packages/python/reelix_core/config.py
+++ packages/python/reelix_core/config.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 
 
-QDRANT_MOVIE_COLLECTION_NAME = "Movies_BGE_AUG-RE"
+QDRANT_MOVIE_COLLECTION_NAME = "Movies_BGE_DEC-AGENT"
 QDRANT_TV_COLLECTION_NAME = "TV_Shows_BGE_AUG-RE"
 
 INTENT_MODEL = "JJTsao/intent-classifier-distilbert-movierec"  # Fine-tuned intent classification model for query intent classifiation
 EMBEDDING_MODEL = "JJTsao/fine-tuned_movie_retriever-bge-base-en-v1.5"  # Fine-tuned sentence transfomer model for dense vector embedding
 RERANKER_MODEL = "JJTsao/movietv-reranker-cross-encoder-base-v1"
+AGENT_MODEL = "gpt-4.1-mini"
 CHAT_COMPLETION_MODEL = "gpt-4o-mini"
+
 
 NLTK_PATH = Path(__file__).resolve().parent.parent / "reelix_models" / "assets" / "nltk_data"
 BM25_PATH = Path(__file__).resolve().parent.parent / "reelix_models" / "assets" / "bm25_files"
