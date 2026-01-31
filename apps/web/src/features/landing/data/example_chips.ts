7472697e4976aa241bc9320cf77b49b917920764
JJ Tsao <35306235+jj-tsao@users.noreply.github.com>
2026-01-30 18:10:45 -0800

    Update example_chips.ts

--- apps/web/src/features/landing/data/example_chips.ts
+++ apps/web/src/features/landing/data/example_chips.ts
@@ -1,7 +1,7 @@
 export const EXAMPLE_CHIPS = [
   "Psychological thrillers with a satirical tone after 2000",
   "Heartwarming coming-of-age drama on Netflix",
-  "I like Inception. What else do you recommend?",
+  "I like Interstellar. What else do you recommend?",
   "Tell me how this works?",
   // "Mind-bending, fast-paced sci-fi on HBO and Hulu",
   "Gritty neo-noirs with stylish actions in the 90s",
